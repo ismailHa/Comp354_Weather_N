28d063680b2ed0c16155c8c0dae78db1c0fe7b9c
Chris Hewlings <chris.hewlings@gmail.com>
2018-11-09 07:22:13 -0500

    Implemented action bar, fixed capability to return to Settings after initial setup

--- app/src/main/java/ca/concordia/comp354mn/comp354mn_project/MainActivity.java
+++ app/src/main/java/ca/concordia/comp354mn/comp354mn_project/MainActivity.java
@@ -7,16 +7,21 @@ import android.os.Handler;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.preference.PreferenceManager;
 import android.widget.TextView;
+import android.widget.Toast;
+
+
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Iterator;
 
 public class MainActivity extends AppCompatActivity {
-
-    boolean showedSplashScreen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +38,32 @@ public class MainActivity extends AppCompatActivity {
         TextView tv1 = (TextView) textViewId;
         tv1.setText(weatherInfo);
 
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_refresh:
+                // Move code from onCreate to here for updating weather content on demand
+                Toast.makeText(this, "Refreshing weather data...", Toast.LENGTH_SHORT)
+                        .show();
+                break;
+            case R.id.action_settings:
+                Intent settingsIntent = new Intent(this, SettingsActivity.class);
+                startActivity(settingsIntent);
+                break;
+            default:
+                break;
+        }
+
+        return true;
     }
 
 }
