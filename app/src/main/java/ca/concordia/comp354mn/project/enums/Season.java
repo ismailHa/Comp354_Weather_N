602b2dd57655db30539c569c37ee503073f0b5c6
Chris Hewlings <chris.hewlings@gmail.com>
2018-12-01 11:44:10 -0500

    Added enumerated types for season & weather condition

---
+++ app/src/main/java/ca/concordia/comp354mn/project/enums/Season.java
@@ -0,0 +1,8 @@
+package ca.concordia.comp354mn.project.enums;
+
+public enum Season {
+    SPRING,
+    SUMMER,
+    FALL,
+    WINTER
+}
---
+++ app/src/main/java/ca/concordia/comp354mn/project/enums/WeatherCondition.java
@@ -0,0 +1,8 @@
+package ca.concordia.comp354mn.project.enums;
+
+public enum WeatherCondition {
+    CLEAR,
+    OVERCAST,
+    RAIN,
+    SNOW
+}
